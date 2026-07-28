151eced6333c242d7583aeae0d1fb743c8b7c44b
tysonsingh <64723420+tysonsingh@users.noreply.github.com>
2026-07-29 03:18:13 +0530

    Time: 13 ms (70.98%), Space: 46.2 MB (80.34%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,27 +1,34 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        String vowels = "aeiou";
         int maxV = 0;
         int ans;
         for(int i = 0; i < k; i++) {
             char curr = s.charAt(i);
-            if(vowels.contains(String.valueOf(curr))) maxV++;
+            if(isVowel(curr)) maxV++;
         }
 
-        ans = maxV > 0 ? maxV : 0;
+        ans = maxV;
 
         for(int i = k ; i < s.length(); i++) {
             //remove
             char prevChar = s.charAt(i-k);
-            if ( vowels.contains(String.valueOf(prevChar)) ) maxV--;
+            if (isVowel(prevChar)) maxV--;
 
             char curr = s.charAt(i);
             //Add 
-            if(vowels.contains(String.valueOf(curr))) maxV++;
+            if(isVowel(curr)) maxV++;
 
             ans = Math.max(maxV, ans);
         }
 
         return ans;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' ||
+               c == 'e' ||
+               c == 'i' ||
+               c == 'o' ||
+               c == 'u';
     }
 }
