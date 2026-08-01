e1bdd606f099f681f17eb40ef4f9eed4add3f3d5
tysonsingh <64723420+tysonsingh@users.noreply.github.com>
2026-08-02 01:21:34 +0530

    Time: 1 ms (52.06%), Space: 42.7 MB (75.02%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+
+        int[][] dp = new int[n][n];
+
+        // Base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        // Fill DP table
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len - 1 < n; i++) {
+
+                int j = i + len - 1;
+
+                int pickLeft = nums[i] - dp[i + 1][j];
+                int pickRight = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = Math.max(pickLeft, pickRight);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}
