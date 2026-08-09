2419b122322153943562e9d9fc88b49467627624
tysonsingh <64723420+tysonsingh@users.noreply.github.com>
2026-08-09 19:17:58 +0530

    Time: 2 ms (2.6%), Space: 46.7 MB (13.94%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        if( mat.length == 1 ) return mat[0][0];
+        int n = mat.length;
+        int total = 0;
+        //Primary Diagonal
+        for(int row = 0; row < mat.length; row++) {
+            for(int column = 0 ; column < mat[0].length; column++) {
+                if(row == column) {
+                    total += mat[row][column];
+                }
+            }
+        }
+
+        for(int row = 0; row < mat.length; row++) {
+            for(int column = 0; column < mat[0].length; column++) {
+                if(column == (n - row - 1)) {
+                    total += mat[row][column];
+                }
+            }
+        }
+
+        if(n % 2 != 0) return total -= mat[n/2][n/2];
+
+        return total;
+    }
+}
