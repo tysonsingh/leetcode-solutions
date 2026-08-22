aeff76db5d0ba796734fedc8147d62615b7dc511
tysonsingh <64723420+tysonsingh@users.noreply.github.com>
2026-08-22 19:15:24 +0530

    Time: 5 ms (24.44%), Space: 45.2 MB (24.93%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,26 +1,44 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        
-        Set<Integer> ans = new HashSet<>();
+    // public int[] intersection(int[] nums1, int[] nums2) {
+    //     int i = 0;
+    //     int j = 0;
+    //     Arrays.sort(nums1);
+    //     Arrays.sort(nums2);
 
-        while(i < nums1.length && j < nums2.length ) {
-            if( nums1[i] < nums2[j]) {
-                i++;
-            }
-            else if( nums1[i] > nums2[j] ) {
-                j++;
-            }
-            else {
-                ans.add(nums1[i]);
-                i++;
-                j++;
+    //     Set<Integer> ans = new HashSet<>();
+
+    //     while(i < nums1.length && j < nums2.length ) {
+    //         if( nums1[i] < nums2[j]) {
+    //             i++;
+    //         }
+    //         else if( nums1[i] > nums2[j] ) {
+    //             j++;
+    //         }
+    //         else {
+    //             ans.add(nums1[i]);
+    //             i++;
+    //             j++;
+    //         }
+    //     }
+
+    //     return ans.stream().mapToInt(Integer::intValue).toArray();
+    // }
+
+    //Optimal
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> hs1 = new HashSet<>();
+        Set<Integer> hs2 = new HashSet<>();
+
+        for(int i = 0; i < nums1.length; i++) {
+            hs1.add(nums1[i]);
+        }
+
+        for(int i = 0; i < nums2.length; i++) {
+            if(hs1.contains(nums2[i])) {
+                hs2.add(nums2[i]);
             }
         }
 
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return hs2.stream().mapToInt(Integer::intValue).toArray();
     }
 }
